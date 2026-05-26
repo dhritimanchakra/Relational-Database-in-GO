38c9271625b8eb18f5fe31ba962c5aec852a8a8c
Dhrimanti <dhritimancg@gmail.com>
2026-05-26 14:00:57 +0530

    Started working in it

---
+++ btree.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/binary"
+)
+
+const (
+	BNODE_NODE = 1
+	BNODE_LEAF = 2
+)
+const BTREE_PAGE_SIZE = 4096
+const BTREE_MAX_KEY_SIZE = 1000
+const BTREE_MAX_VAL_SIZE = 3000
+
+type BNode []byte
+
+func assert(cond bool) {
+	if !cond {
+		panic("assertation failed")
+	}
+}
+
+func init() {
+	node1max := 4 + 1*8 + 1*2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+	assert(node1max <= BTREE_PAGE_SIZE)
+}
+
+func (node BNode) btype() uint16 {
+	return binary.LittleEndian.Uint16(node[0:2])
+}
+
+func (node BNode) nkeys() uint16 {
+	return binary.LittleEndian.Uint16(node[2:4])
+}
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+	binary.LittleEndian.PutUint16(node[0:2], btype)
+	binary.LittleEndian.PutUint16(node[2:4], nkeys)
+}
+
+func (node BNode) getPtr(idx uint16) uint64 {
+	assert(idx < node.nkeys())
+	pos := 4 + 8*idx
+	return binary.LittleEndian.Uint64(node[pos:])
+}
+
+func (node BNode) setPtr(idx uint16, val uint64) {
+	assert(idx < node.nkeys())
+	pos := 4 + 8*idx
+	binary.LittleEndian.PutUint64(node[pos:], val)
+}
+
+func (node BNode) getOffset(idx uint16) uint16 {
+	if idx == 0 {
+		return 0
+	}
+	pos := 4 + 8*node.nkeys() + 2*(idx-1)
+	return binary.LittleEndian.Uint16(node[pos:])
+}
